0fd4d4be64cb7f2c493ffeee4359a22b5e1239ab
vikassaroj123 <118330770+vikassaroj123@users.noreply.github.com>
2024-04-14 16:36:01 +0530

    Fix Error

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -5,7 +5,6 @@ import emailjs from '@emailjs/browser';
 import { useState } from 'react';
 import { TbMailForward } from "react-icons/tb";
 import { toast } from 'react-toastify';
-import { PopupWidget } from 'react-calendly';
 
 function ContactForm() {
   const [input, setInput] = useState({
@@ -128,17 +127,6 @@ function ContactForm() {
         </div>
 
         <div>
-          <PopupWidget
-            url="https://calendly.com/your_scheduling_page"
-            /*
-             * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-             * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-             */
-            rootElement={document.getElementById("root")}
-            text="Click here to schedule!"
-            textColor="#ffffff"
-            color="#00a2ff"
-          />
         </div>
 
       </div>
