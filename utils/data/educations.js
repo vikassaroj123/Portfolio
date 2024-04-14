27538211d3404388c30e0dee8f9e2f8f8bd12d7b
vikassaroj123 <118330770+vikassaroj123@users.noreply.github.com>
2024-04-14 16:48:55 +0530

    Update Education Components

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of technology specialization in Information Technology",
+    duration: "2021 - Present",
+    institution: "JSS Academy of Technical Education, C-20/1, C Block, Phase 2, Industrial Area, Sector 62, Noida, Uttar Pradesh 201301",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
     duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    institution: "MGS Inter College, Sultanpur, Uttar Pradesh, 228151, India.",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    institution: "Saraswati Vidya Mandir, Sultapur",
   }
 ]
