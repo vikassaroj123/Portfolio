84af469b2f73ef7572ca32e400db0793e2d56ee4
VIKAS SAROJ <118330770+vikassaroj123@users.noreply.github.com>
2024-07-26 21:44:53 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,9 @@
 export const experiences = [
    {
-    id: 4,
+    id: 3,
     title: "Software Engineer",
     company: "Mutagen",
     duration: "(July 2024 - Present)"
-  },
-  {
-    id: 3,
-    title: "React Developer",
-    company: "House Technologies",
-    duration: "(May 2024 - July 2024)"
   },
   {
     id: 2,
@@ -19,8 +13,8 @@ export const experiences = [
   },
   {
     id: 1,
-    title: 'MERN Stack Developer',
-    company: "Code Clause",
-    duration: "(June 2023 - Aug 2023)"
-  }
+    title: "React Developer",
+    company: "House Technologies",
+    duration: "(May 2024 - July 2024)"
+   }
 ]
